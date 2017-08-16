161d8ce57b7e52272c3aaeddfa03b640ea02a07b
cb-dtn <dtn@chargebee.com>
2017-08-16 23:30:51 +0530

    happy coding

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,15 @@
 $(document).ready(function(){
-	$(window).scroll(function() {
-	    var scroll = $(window).scrollTop();
-	    if (scroll >= 1) {
-	        $(".me-header").addClass("me-header--affix");
-	    } else {
-	    	$(".me-header--affix").removeClass("me-header--affix");
-	    }
+	//open preview
+	$('[data-preview-open="true"]').click(function(){
+		var _parent = $(this).parents('[data-preview-project="true"]')[0];
+		$(_parent).find('[data-preview-frame="true"]').addClass('ui-section__frame--open');
+		$(_parent).find('[data-preview-close="true"]').addClass('ui-section__close--open');
 	});
 
-	$('a[href*=#]:not([href=#])').click(function() {
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-	      	var target = $(this.hash);
-	      	target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	      	if (target.length) {
-	        	$('html,body').animate({
-	          		scrollTop: target.offset().top - 54
-	        	}, 500);
-	        	return false;
-	      	}
-	    }
+	//close preview
+	$('[data-preview-close="true"]').click(function(){
+		var _parent = $(this).parents('[data-preview-project="true"]')[0];
+		$(_parent).find('[data-preview-frame="true"]').removeClass('ui-section__frame--open');
+		$(_parent).find('[data-preview-close="true"]').removeClass('ui-section__close--open');
 	});
 });
-
-//typeform enquiry script
-(function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
