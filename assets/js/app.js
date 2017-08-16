3b2bd55c89eafd06b9eef9292201962c247d700b
cb-dtn <dtn@chargebee.com>
2017-08-17 03:06:40 +0530

    js and ui

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
 	//open preview
-	$('[data-preview-open="true"]').click(function(){
-		var _parent = $(this).parents('[data-preview-project="true"]')[0];
-		$(_parent).find('[data-preview-frame="true"]').addClass('ui-section__frame--open');
-		$(_parent).find('[data-preview-close="true"]').addClass('ui-section__close--open');
+	$('[data-preview-project="true"]').click(function(){		
+		$(this).find('[data-preview-frame="true"]').addClass('ui-section__frame--open');
+		$('[data-preview-close="true"]').addClass('ui-section__close--open');
 		$('body').addClass('overflow-hidden');
 	});
 
 	//close preview
-	$('[data-preview-close="true"]').click(function(){
-		var _parent = $(this).parents('[data-preview-project="true"]')[0];
-		$(_parent).find('[data-preview-frame="true"]').removeClass('ui-section__frame--open');
-		$(_parent).find('[data-preview-close="true"]').removeClass('ui-section__close--open');
+	$('[data-preview-close="true"]').click(function(){		
+		$('[data-preview-frame="true"]').removeClass('ui-section__frame--open');
+		$('[data-preview-close="true"]').removeClass('ui-section__close--open');
 		$('body').removeClass('overflow-hidden');
 	});
 });
