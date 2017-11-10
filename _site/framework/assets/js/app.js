480d14ccd36e378fe93665692e27b0a3fcd3f12b
cb-dtn <dtn@chargebee.com>
2017-11-10 19:02:46 +0530

    ui

--- _site/framework/assets/js/app.js
+++ _site/framework/assets/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('[fw-demo-trigger="true"]').each(function(){
     	_thisParent = $(this).parents('[fw-demo="true"]')[0];
         _thisHtml = $(_thisParent).find('[fw-demo-preview="true"]').html()
+        _thisHtml = _thisHtml.replace(new RegExp("<!---->", 'g'),"\n");
         $(_thisParent).find('[fw-demo-code="true"]').text(_thisHtml);
     });
 });
