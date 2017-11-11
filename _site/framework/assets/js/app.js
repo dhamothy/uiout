eda221d78caf1a72680b360e560ca77ee7deedd3
cb-dtn <dtn@chargebee.com>
2017-11-11 21:27:58 +0530

    jekyll filters

--- _site/framework/assets/js/app.js
+++ _site/framework/assets/js/app.js
@@ -1,8 +1,9 @@
-$(document).ready(function(){
-    $('[fw-demo-trigger="true"]').each(function(){
-    	_thisParent = $(this).parents('[fw-demo="true"]')[0];
-        _thisHtml = $(_thisParent).find('[fw-demo-preview="true"]').html()
-        _thisHtml = _thisHtml.replace(new RegExp("<!---->", 'g'),"\n");
-        $(_thisParent).find('[fw-demo-code="true"]').text(_thisHtml);
-    });
-});
+// $(document).ready(function(){
+//     $('[fw-demo="true"]').each(function(){
+//     	// _thisParent = $(this).parents('[fw-demo="true"]')[0];
+//     	_thisParent = $(this);
+//         _thisHtml = $(_thisParent).find('[fw-demo-preview="true"]').html()
+//         _thisHtml = _thisHtml.replace(new RegExp("<!---->", 'g'),"\n");
+//         $(_thisParent).find('[fw-demo-code="true"]').text(_thisHtml);
+//     });
+// });
